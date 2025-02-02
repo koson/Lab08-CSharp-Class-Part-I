b91d39098dfab48e8bcde6485c269e28fba79f54
senashino <phwn26@gmail.com>
2025-02-02 19:17:35 +0700

    update

---
+++ Lab08_Ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Person
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public int Income { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person p = new Person();
+        p.Name = "Rambo";
+        p.Id = "1987";
+        p.Income = 2500;
+
+        // แสดงชนิดของอ็อบเจ็กต์และชนิดของตัวแปร
+        Console.WriteLine($"Type of p is {p.GetType()}");
+        Console.WriteLine($"p.Name = {p.Name}\ttype = {p.Name.GetType()}");
+        Console.WriteLine($"p.Id = {p.Id}\ttype = {p.Id.GetType()}");
+        Console.WriteLine($"p.Income = {p.Income}\ttype = {p.Income.GetType()}");
+    }
+}
+
